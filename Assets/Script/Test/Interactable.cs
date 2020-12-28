11b8add90b9dc8c9b78dcf3dd1d6f66345b78596
GDLAB-MSI05\GD Student <jjc1114@sina.com>
2020-12-28 18:27:50 +0800

    Add Interactive Text

--- Assets/Script/Test/Interactable.cs
+++ Assets/Script/Test/Interactable.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Interactable : MonoBehaviour
 {
     SpriteRenderer sr;
     public Material outlineMaterial;
     public Material defaultMaterial;
+    public Transform textPosition;
+
+    public GameObject TextUI;
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
         defaultMaterial = sr.material;
+        TextUI.SetActive(false);
     }
 
     //碰到玩家时，给outline
@@ -19,6 +24,8 @@ public class Interactable : MonoBehaviour
         if (other.gameObject.GetComponent<Player_Controller>() != null)
         {
             sr.material = outlineMaterial;
+            TextUI.transform.position = textPosition.position;
+            TextUI.SetActive(true);
         }
     }
 
@@ -27,6 +34,7 @@ public class Interactable : MonoBehaviour
         if (other.gameObject.GetComponent<Player_Controller>() != null)
         {
             sr.material = defaultMaterial;
+            TextUI.SetActive(false);
         }
     }
 }
