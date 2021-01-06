dbe050e104bd7bf284d5fb42e75e60dc6a8faa92
GDLAB-MSI09\GD Student <zhangyijie16@163.com>
2021-01-07 00:36:46 +0800

    update

--- Assets/Script/Test/BaseUnit.cs
+++ Assets/Script/Test/BaseUnit.cs
@@ -12,18 +12,22 @@ public class BaseUnit : MonoBehaviour
     //This is a professional way of storing a reference to Components
     protected Rigidbody2D rb;
     ////Animator anim;
-    SpriteRenderer sr;
+    protected SpriteRenderer sr;
+    protected Animator anim;
+    public Transform trans;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        ////anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
+        trans = GetComponent<Transform>();
+
     }
 
     //make it protected so we can call it from playercontroller and AIcontroller
 
-
+        //翻转
     protected void Flip(float DirectionX)
     {
         if (DirectionX < 0)
@@ -48,12 +52,74 @@ public class BaseUnit : MonoBehaviour
         //keep the y-velocity at it original value
 
         rb.velocity = new Vector2(DirectionX * speed, DirectionY * speed);
-
+        anim.SetFloat("Speed", Mathf.Abs(DirectionX)+Mathf.Abs(DirectionY));
         //send current movement to the animator
         //this way,the animator can handle which animation to play 
         //send the absolute direction
         //because our animator needs positive numbers for the transition
 
         ////anim.SetFloat("Speed", Mathf.Abs(direction));
+    }
+
+
+    //选择武器，根据输入选择启用武器，并禁用其余武器，若不可用则无法选择
+    protected void WeaponSwitch()
+    {
+        if (Input.GetButtonDown("Weapon1"))
+        {
+            GameObject.Find("Weapon1").GetComponent<Weapon1>().enabled = true;
+            GameObject.Find("Weapon2").GetComponent<Weapon2>().enabled = false;
+            GameObject.Find("Weapon3").GetComponent<Weapon3>().enabled = false;
+            GameObject.Find("Weapon4").GetComponent<Weapon4>().enabled = false;
+            anim.SetBool("Weapon1", true);
+            anim.SetBool("Weapon2", false);
+            anim.SetBool("Weapon3", false);
+            anim.SetBool("Weapon4", false);
+        }
+        if (Input.GetButtonDown("Weapon2"))
+        {
+            if (GameObject.Find("Weapon2").GetComponent<Weapon2>().isAvailable==true)
+            {
+                GameObject.Find("Weapon1").GetComponent<Weapon1>().enabled = false;
+                GameObject.Find("Weapon2").GetComponent<Weapon2>().enabled = true;
+                GameObject.Find("Weapon3").GetComponent<Weapon3>().enabled = false;
+                GameObject.Find("Weapon4").GetComponent<Weapon4>().enabled = false;
+                anim.SetBool("Weapon1", false);
+                anim.SetBool("Weapon2", true);
+                anim.SetBool("Weapon3", false);
+                anim.SetBool("Weapon4", false);
+            }
+        }
+        if (Input.GetButtonDown("Weapon3"))
+        {
+            if (GameObject.Find("Weapon3").GetComponent<Weapon3>().isAvailable == true)
+            {
+                GameObject.Find("Weapon1").GetComponent<Weapon1>().enabled = false;
+                GameObject.Find("Weapon2").GetComponent<Weapon2>().enabled = false;
+                GameObject.Find("Weapon3").GetComponent<Weapon3>().enabled = true;
+                GameObject.Find("Weapon4").GetComponent<Weapon4>().enabled = false;
+                anim.SetBool("Weapon1", false);
+                anim.SetBool("Weapon2", false);
+                anim.SetBool("Weapon3", true);
+                anim.SetBool("Weapon4", false);
+            }
+                
+        }
+        if (Input.GetButtonDown("Weapon4"))
+        {
+            if (GameObject.Find("Weapon3").GetComponent<Weapon3>().isAvailable == true)
+            {
+                GameObject.Find("Weapon1").GetComponent<Weapon1>().enabled = false;
+                GameObject.Find("Weapon2").GetComponent<Weapon2>().enabled = false;
+                GameObject.Find("Weapon3").GetComponent<Weapon3>().enabled = false;
+                GameObject.Find("Weapon4").GetComponent<Weapon4>().enabled = true;
+                anim.SetBool("Weapon1", false);
+                anim.SetBool("Weapon2", false);
+                anim.SetBool("Weapon3", false);
+                anim.SetBool("Weapon4", true);
+            }
+                
+        }
+
     }
 }
---
+++ Assets/Script/Test/Weapon/WeaponBase.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBase : MonoBehaviour
+{
+    public float WeaponRange;       //检测范围
+    public bool isAvailable = false;//是否已拥有
+    public float cooldown;
+    public float timer;
+    public bool isActive;           //是否已冷却
+    public LayerMask enemylayers;
+    public LayerMask itemlayers;
+
+    protected Animator anim;
+    protected Transform playerTrans;
+    public Transform attackPoint;
+
+    //protected GameObject[] enemyArr;
+    protected List<float> enemyList;
+    protected Dictionary<float, GameObject> enemyDic;
+
+    protected List<float> itemList;
+    protected Dictionary<float, GameObject> itemDic;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+        playerTrans = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+
+        enemyDic = new Dictionary<float, GameObject>();//初始化       
+        enemyList = new List<float>();
+
+        itemDic = new Dictionary<float, GameObject>();//初始化       
+        itemList = new List<float>();
+
+    }
+
+
+
+
+    protected void DetectEnemy()
+    {
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, WeaponRange, enemylayers);
+        Debug.Log(hitEnemies.Length);
+        if(hitEnemies.Length > 0)
+        {
+            for (int i = 0; i < hitEnemies.Length; i++)
+            {
+                float dis = Vector3.Distance(hitEnemies[i].gameObject.transform.position, playerTrans.position);
+                enemyDic.Add(dis, hitEnemies[i].gameObject);
+                Debug.Log(dis);
+                if (!enemyList.Contains(dis))
+                {
+                    enemyList.Add(dis);
+                }
+            }
+            enemyList.Sort();//对距离进行排序
+            Debug.Log("***" + enemyList[0]);
+            GameObject obj;
+            enemyDic.TryGetValue(enemyList[0], out obj);//获取距离最近的对象
+            Debug.Log(obj.name);
+            enemyList.Clear();
+            enemyDic.Clear();
+        } 
+    }
+
+    protected void DetectItem()
+    {
+
+        Collider2D[] hitItems = Physics2D.OverlapCircleAll(attackPoint.position, WeaponRange, itemlayers);
+        Debug.Log(hitItems.Length);
+        if (hitItems.Length > 0)
+        {
+            for (int i = 0; i < hitItems.Length; i++)
+            {
+                float dis = Vector3.Distance(hitItems[i].gameObject.transform.position, playerTrans.position);
+                itemDic.Add(dis, hitItems[i].gameObject);
+                Debug.Log(dis);
+                if (!itemList.Contains(dis))
+                {
+                    itemList.Add(dis);
+                }
+            }
+            itemList.Sort();//对距离进行排序
+            Debug.Log("***" + itemList[0]);
+            GameObject obj;
+            itemDic.TryGetValue(itemList[0], out obj);//获取距离最近的对象
+            Debug.Log(obj.name);
+            itemList.Clear();
+            itemDic.Clear();
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, WeaponRange);
+    }
+}
